26bea9a8ade886ba1a878173937299a453524d38
Du Guangting <hello@delayer.network>
2024-06-01 22:17:58 +0200

    feat: add relaunch info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,9 @@ export default function Home() {
             <p className="text-6xl sm:text-7xl text-accent  uppercase sm:w-[564px]">
               De Layer
             </p>
-            <p className="text-6xl sm:text-7xl sm:w-[564px]">
-              COMING SOON
-            </p>
-            <div className="min-h-10 mt-2">
-              <CountdownTimer targetDate={futureDate} />
-            </div>
+            <blockquote className="twitter-tweet">
+              <p lang="en" dir="ltr">Relaunch of De Layer<br /><br />Dear community, we are officially announcing the relaunch of De Layer token.<br /><br />We are still in the process of working out the details of the relaunch. <br /><br />Here are the things we have decided on now:<br />- The token will be relaunched on a new smart contract<br />-…</p>&mdash; De δ (@delayerevm) <a href="https://twitter.com/delayerevm/status/1796995459203866767?ref_src=twsrc%5Etfw">June 1, 2024</a></blockquote>
+            <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
           </div>
         </div>
       </div>
